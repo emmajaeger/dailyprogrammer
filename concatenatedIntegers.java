9a9de14627bfe525734af8bfb9258711f2212c3c
Emma Jaeger <emmajaeger@berkeley.edu>
2017-06-22 16:31:38 -0700

    tried concatenated integers

---
+++ concatenatedIntegers.java
@@ -0,0 +1,17 @@
+// [2017-05-08] Challenge #314 [Easy] Concatenated Integers
+/* I'm going to start with a simplified version first using ony 3 arguments and allowing leading
+0s, then maybe I'll try to do the actual version later on. */
+
+public class concatenatedIntegers {
+	public static void main(String [] args) {
+
+		if (args.length < 3) {
+			System.out.println('3 arguments required!');
+			return;
+		}
+
+		for (int i = 0; i < args.length; i++) {
+			largest = Integer.parseInt(args[i] + args[]) // some way to check all permutations?
+		}
+	}
+}
